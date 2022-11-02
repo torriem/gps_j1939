6c8d468f1757d38b4015e5c6c7af8932cdcc3edd
Michael Torrie <torriem@gmail.com>
2022-11-01 21:54:55 -0600

    moved some extern declarations to the cpp file, added yaw rate calcs and filtering on roll and yaw rate.

--- px1172rh.cpp
+++ px1172rh.cpp
@@ -2,6 +2,19 @@
 #include "stinmea.h"
 #include "haversine.h"
 	
+extern double antenna_forward;
+extern double antenna_height;
+extern double antenna_right;
+
+extern double autosteer_lat;
+extern double autosteer_lon;
+extern double autosteer_heading;
+extern double autosteer_roll;
+extern double autosteer_yawrate;
+extern double autosteer_speed;
+extern double autosteer_altitude;
+extern char autosteer_mode;
+
 uint8_t year=0, month=0, day=0;
 uint8_t hour=0, minute=0, seconds=0, hundredths=0;
 long latitude=0;
@@ -15,8 +28,25 @@ double lon=0;
 bool got_pos=false;
 bool got_attitude=false;
 
+float last_heading = 0;
+uint32_t last_heading_time = 0;
+
 char nmea_buffer[160];
-STINMEA sti(nmea_buffer, sizeof(nmea_buffer));
+//char nmea_buffer1[160];
+STINMEA sti(nmea_buffer, sizeof(nmea_buffer)); //handles PSTI
+//SimpleNMEAParser gga(nmea_buffer1, sizeof(nmea_buffer1); //handles GGA
+
+float yaw_P = 1.0f;
+float yaw_varProcess = 0.0003f;
+float yaw_Xe = 0;
+
+float heading_P = 1.0f;
+float heading_varProcess = 0.0003f;
+float heading_Xe = 0;
+
+float roll_P = 1.0f;
+float roll_varProcess = 0.0003f;
+float roll_Xe = 0;
 
 bool psti_process(char c) {
 	if (sti.process(c)) {
@@ -90,9 +120,23 @@ bool psti_process(char c) {
 			got_attitude = true;
 			//heading is 90 degress off of the dual antenna heading
 			heading90 = sti.getHeading();
-			heading = (heading90 - 9000);
+
+			/*
+			heading_Pc = heading_P + heading_varProcess;
+			heading_G = heading_Pc / (heading_Pc / heading90);
+			heading_P = (1 - heading_G) * heading_Pc;
+			heading_Xp = heading_Xe;
+			heading_Zp = heading_Xp;
+			heading_Xe = (heading_G * (yaw_rate - heading_Zp)) + heading_Xp;
+			heading90 = heading_Xe; //hopefully filtered heading
+			*/
+
+			heading = (heading90 + 9000);
 			if (heading < 0)
 				heading += 36000;
+			if (heading >= 36000)
+				heading -= 36000;
+
 			//heading = sti.getHeading();
 			pitch = sti.getPitch();
 		}
@@ -102,17 +146,45 @@ bool psti_process(char c) {
 			double alt_offset2;
 			double center_offset;
 			double roll;
+			float yaw_rate;
+
+			float roll_Pc, roll_G, roll_Xp, roll_Zp; //for filtering yaw rate
+			float yaw_Pc, yaw_G, yaw_Xp, yaw_Zp; //for filtering yaw rate
+			float heading_delta;
+
 			//we need to do some trigonometry so unpack the values
 			//to doubles
 			lat = latitude / 10000000.0;
 			lon = longitude / 10000000.0;
-			roll = pitch / 100.0;
+			roll = -pitch / 100.0;
+
+			//filter roll
+			roll_Pc = roll_P + roll_varProcess;
+			roll_G = roll_Pc / (roll_Pc / roll);
+			roll_P = (1 - roll_G) * roll_Pc;
+			roll_Xp = roll_Xe;
+			roll_Zp = roll_Xp;
+			roll_Xe = (roll_G * (roll - roll_Zp)) + roll_Xp;
+			roll = roll_Xe; //hopefully this is right
+
+			heading_delta = heading - last_heading;
+			if (heading_delta > 180) heading_delta -= 360; //yawing to the left across 0
+			else if (heading_delta <= -180) heading_delta += 360; //yawing to right across 0
+			yaw_rate = heading_delta / ((last_heading_time - millis()) * 1000); //degrees/second
+			last_heading_time = millis();
+			yaw_Pc = yaw_P + yaw_varProcess;
+			yaw_G = yaw_Pc / (yaw_Pc / roll);
+			yaw_P = (1 - yaw_G) * yaw_Pc;
+			yaw_Xp = yaw_Xe;
+			yaw_Zp = yaw_Xp;
+			yaw_Xe = (yaw_G * (yaw_rate - yaw_Zp)) + yaw_Xp;
+			autosteer_yawrate = yaw_Xe; //hopefully filtered yaw rate to send back to main program
 
 			//offset from the tilt of the tractor
 			tilt_offset = sin(roll * haversine::toRadians) * antenna_height;
 
 			//account for distance from left antenna to center of tractor
-			center_offset = cos(roll * haversine::toRadians) * antenna_left;
+			center_offset = cos(roll * haversine::toRadians) * antenna_right;
 
 			//calculate ground-level elevation in center of tractor,
 			//relative to left antenna
@@ -149,6 +221,10 @@ bool psti_process(char c) {
 			}
 		}
 	}
+
+	//if (gga.process(c) {
+	//
+	//}
 
 	return false;
 }
