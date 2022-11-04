40710167d39e00decfbef03e743ab2f3056862a4
Michael Torrie <torriem@gmail.com>
2022-11-04 13:42:09 -0600

    using nmea log file and replay.py to test teensy code on bench

--- dualtest.py
+++ dualtest.py
@@ -7,7 +7,7 @@ import math
 
 INCHES = 0.0254 # inches per meter
 GPS_HEIGHT = 120 * INCHES # distance above axle to primary antenna
-GPS_LEFT = 53 / 2.0 * INCHES # lateral distance from primary antenna to center of axle
+GPS_RIGHT = 53 / 2.0 * INCHES # lateral distance from right antenna to center of axle
 GPS_FORWARD = 0 * INCHES # distance in front of axle
 
 last_gps_pos = None
@@ -42,7 +42,7 @@ def virtual_position (gps_pos, roll, heading, side_heading = None):
     #if abs(roll) < 0.01: return gps_pos
 
     if side_heading is None:
-        side_heading = (heading + 90) % 360 #90 degrees to right of vehicle heading
+        side_heading = (heading - 90) % 360 #90 degrees to left of vehicle heading
 
     lat_offset = math.sin(math.radians(roll)) * GPS_HEIGHT
     #print (side_heading, lat_offset / INCHES)
@@ -51,30 +51,30 @@ def virtual_position (gps_pos, roll, heading, side_heading = None):
     #print (alt_offset / INCHES)
 
 
-    if GPS_LEFT:
-        additional_offset = math.cos(math.radians(roll)) * GPS_LEFT
-        print ("Heading %f, offset %f, additional_offset %f" % (side_heading, lat_offset, additional_offset))
+    if GPS_RIGHT:
+        additional_lat_offset = math.cos(math.radians(roll)) * GPS_RIGHT
+        additional_alt_offset = math.sin(math.radians(roll)) * GPS_RIGHT
+    else:
+        additional_lat_offset = 0
+        additional_alt_offset = 0
 
-    new_pos1 = haversines.at_distance_bearing( (gps_pos[0], gps_pos[1]), side_heading, lat_offset + additional_offset)
+    #print ("Heading %f, offset %f, additional_offset %f" % (side_heading, lat_offset, additional_lat_offset))
+    #print ("lat offset total: %f" % (lat_offset + additional_lat_offset))
+    #print ("altitude %f, offset %f, additional_offset %f" % (gps_pos[2], alt_offset, additional_alt_offset))
+    new_pos = haversines.at_distance_bearing( (gps_pos[0], gps_pos[1]), side_heading, lat_offset + additional_lat_offset)
+    new_alt = gps_pos[2] - alt_offset + additional_alt_offset
 
-    new_pos = haversines.at_distance_bearing( (gps_pos[0], gps_pos[1]), side_heading, lat_offset)
 
-
+ 
     # translate forward or back to axle from antenna, ignoring pitch of vehicle
     if GPS_FORWARD:
         new_pos = haversines.at_distance_bearing( new_pos, heading, -GPS_FORWARD)
 
-    if GPS_LEFT:
-        new_pos = haversines.at_distance_bearing( new_pos, side_heading, GPS_LEFT)
-
-    print (haversines.distance( (gps_pos[0], gps_pos[1]), new_pos))
-    print (lat_offset+additional_offset)
-    
     #print (new_pos)
     #print (new_pos1)
     #print ()
 
-    return ( new_pos[0], new_pos[1], gps_pos[2] - alt_offset )
+    return ( new_pos[0], new_pos[1], new_alt )
 
 def log_gps( gps_pos, position, logfile):
     global last_gps_pos
@@ -107,6 +107,41 @@ if __name__ == "__main__":
 
     queue = queue.Queue(1000)
     quit_threads = False
+
+    class KFilter:
+        def __init__(self, P, process, var):
+            self.P = P
+            self.varProcess = process
+            self.var = var
+
+            self.Xe = 0
+
+        def filter(self, reading):
+            Pc = self.P + self.varProcess  #error estimate
+            G = Pc / (Pc + self.var) # gain
+            self.P = (1 - G) * Pc
+            Xp = self.Xe
+            Zp = Xp
+            self.Xe = (G * (reading - Zp)) + Xp
+
+            return self.Xe
+
+    class KFilter1:
+        def __init__(self, meas_error, est_error, q):
+            self.err_measure = meas_error
+            self.err_estimate = est_error
+            self.q = q
+            self.cur_est = 0
+            self.last_est = 0
+            self.gain = 0
+
+        def filter( self, mea):
+            self.gain = self.err_estimate / (self.err_estimate + self.err_measure)
+            self.cur_est = self.last_est + self.gain * (mea - self.last_est)
+            self.err_estimate = (1.0 - self.gain) * self.err_estimate + abs(self.last_est - self.cur_est) * self.q
+            self.last_est = self.cur_est
+            return self.cur_est
+    
 
     class Position:
         varProcess = 0.0003
@@ -141,27 +176,35 @@ if __name__ == "__main__":
     #file = open(sys.argv[1], encoding='utf-8')
 
     if len(sys.argv) > 1:
-        serialport = sys.argv[1]
+        #serialport = sys.argv[1]
+        inputfile = open(sys.argv[1],'r')
+
     else:
         serialport = '/dev/ttyUSB3'
+        serial = serial.Serial(serialport, baudrate = 115200)
 
-    serial = serial.Serial(serialport, baudrate = 115200)
-
-    thread = threading.Thread(target=serial_readline, args = (serial, ),).start()
+        thread = threading.Thread(target=serial_readline, args = (serial, ),).start()
 
     roll = 0
+    last_heading = 0
 
-    while True:
-        if queue.empty(): continue
-        line = queue.get(True, 1)
+    #yaw_filter = KFilter(1.0, 0.0003, 0.1)
+    yaw_filter = KFilter1(0.1, 1.0, 0.001)
+    roll_filter = KFilter1(0.1, 1.0, 0.001)
+    
+    #while True:
+    for line in inputfile:
+        #if queue.empty(): continue
+        #line = queue.get(True, 1)
 
-        log_serial_data(seriallog, line)
+        line = line.strip()
+        #log_serial_data(seriallog, line)
         try:
             msg = pynmea2.parse(line)
 
-            # we need both a GGA and a STI032 that line up
+            # we need both a STI030 and STI036 that line up
 
-            if isinstance(msg, pynmea2.types.GGA):
+            if isinstance(msg, pynmea2.types.proprietary.sti.STI030):
                 if not (position.time == msg.time and position.date == msg.date):
                     # start a new position
                     position.attitude = None
@@ -173,8 +216,7 @@ if __name__ == "__main__":
                 if position.pos and position.attitude:
                     print ("New position.")
 
-            # 035 or 032? AOG uses 032 but docs are fuzzy
-            if isinstance(msg, pynmea2.types.proprietary.sti.STI032):
+            if isinstance(msg, pynmea2.types.proprietary.sti.STI036):
                 if not (position.time == msg.time and position.date == msg.date):
                     # start a new position
                     position.pos = None
@@ -184,32 +226,40 @@ if __name__ == "__main__":
                 position.attitude = msg
 
                 # vehicle heading is 90 degrees less than this reported heading
-                if position.attitude.bearing: #bearing is always right-hand moving base towards left-hand rover
-                    position.attitude.heading = (position.attitude.bearing + 90) % 360
-                    position.attitude.pitch = math.atan(position.attitude.elevation / position.attitude.dist)
+                position.attitude.heading90 = position.attitude.heading
+                position.attitude.heading = (position.attitude.heading90 + 90) % 360
 
-                    # TODO: filter roll just like AOG does
-                    Pc = position.P + Position.varProcess
-                    G = Pc / (Pc + varRoll)
-                    position.P = (1 - G) * Pc
-                    Xp = position.XeRoll
-                    Zp = Xp
-                    position.XeRoll = (G * (position.attitude.pitch - Zp)) + Xp
-                    position.attitude.pitch = position.XeRoll
+                #print (position.attitude.pitch)
 
-                    # might need to negate the pitch
-                    print ('vehicle heading %f, pitch %f, height diff %f' % (position.attitude.heading, position.attitude.pitch, position.attitude.elevation))
+                #position.attitude.pitch = roll_filter.filter(position.attitude.pitch)
 
-                else:
-                    position.attitude.heading = 0
-                    position.attitude.pitch = 0
+                # TODO: filter roll just like AOG does
+                ##Pc = position.P + Position.varProcess
+                ##G = Pc / (Pc + Position.varRoll)
+                ##position.P = (1 - G) * Pc
+                ##Xp = position.XeRoll
+                ##Zp = Xp
+                ##position.XeRoll = (G * (position.attitude.pitch - Zp)) + Xp
+                ##position.attitude.roll = position.XeRoll
+
+                #print ('vehicle heading %f, roll %f, orig altitude %f' % (position.attitude.heading, position.attitude.roll, position.pos.altitude))
 
                 if position.pos and position.attitude:
                     #print (position.pos.longitude, position.pos.latitude)
+                    
+                    heading_delta = position.attitude.heading - last_heading
+                    if heading_delta >180: heading_delta -= 360
+                    if heading_delta <=-180: heading_delta += 360
+                    yaw_rate = heading_delta / 0.2
 
-                    compensated_pos = virtual_position( (position.pos.longitude, position.pos.latitude, position.pos.altitude), position.attitude.pitch, position.attitude.heading, position.attitude.bearing)
-                    print (position.attitude.heading, position.attitude.pitch, compensated_pos)
-                    log_gps( compensated_pos, position, logfile)
+                    #yaw_rate = yaw_filter.filter(yaw_rate)
+
+                    last_heading = position.attitude.heading
+
+                    compensated_pos = virtual_position( (position.pos.longitude, position.pos.latitude, position.pos.altitude), position.attitude.roll, position.attitude.heading, position.attitude.heading90)
+                    #print (position.attitude.heading, position.attitude.roll, compensated_pos)
+                    print ( "%.7f, %.7f, %f, hdg %f, %f, %f" % ( *compensated_pos, position.attitude.heading, position.attitude.pitch, yaw_rate))
+                    #log_gps( compensated_pos, position, logfile)
 
             #print(repr(msg))
         except pynmea2.ParseError as e:
