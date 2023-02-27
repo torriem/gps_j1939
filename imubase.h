25eef8f242525998673c7f3d4ab5e31c591f4e8a
Michael Torrie <torriem@gmail.com>
2023-02-26 18:50:59 -0700

    Base imu class, and bnorvc implmentation.  Hopefully makes it easier to support other kinds of IMUs in the future.

---
+++ imubase.h
@@ -0,0 +1,14 @@
+#ifndef _IMUBASE_H__
+#define _IMUBASE_H__
+
+class IMUBase {
+public:
+	IMUBase();
+
+	virtual void process_data(void) = 0;
+	virtual float get_roll(int lookback_ms) = 0;
+	virtual float get_yaw(int lookback_ms) = 0;
+	virtual float get_pitch(int lookback_ms) = 0;
+};
+
+#endif
