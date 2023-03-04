82d8035b4169517c5c81e8eceaeb5aca8062e3e2
Michael Torrie <torriem@gmail.com>
2023-03-03 21:35:25 -0700

    Added back optional LCD output routines for Teensy and a TFT screen.  To enable, uncomment #define in lcd.h

---
+++ font_LiberationMono.h
@@ -0,0 +1,36 @@
+#ifndef _ST7735_t3_font_LiberationMono_
+#define _ST7735_t3_font_LiberationMono_
+
+#include "whichteensy.h"
+
+#ifdef TEENSY //skip all this if not using Teensy
+#include "ST7735_t3.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const ST7735_t3_font_t LiberationMono_8;
+extern const ST7735_t3_font_t LiberationMono_9;
+extern const ST7735_t3_font_t LiberationMono_10;
+extern const ST7735_t3_font_t LiberationMono_11;
+extern const ST7735_t3_font_t LiberationMono_12;
+extern const ST7735_t3_font_t LiberationMono_13;
+extern const ST7735_t3_font_t LiberationMono_14;
+extern const ST7735_t3_font_t LiberationMono_16;
+extern const ST7735_t3_font_t LiberationMono_18;
+extern const ST7735_t3_font_t LiberationMono_20;
+extern const ST7735_t3_font_t LiberationMono_24;
+extern const ST7735_t3_font_t LiberationMono_28;
+extern const ST7735_t3_font_t LiberationMono_32;
+extern const ST7735_t3_font_t LiberationMono_40;
+extern const ST7735_t3_font_t LiberationMono_48;
+extern const ST7735_t3_font_t LiberationMono_60;
+extern const ST7735_t3_font_t LiberationMono_72;
+extern const ST7735_t3_font_t LiberationMono_96;
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+#endif
+#endif
